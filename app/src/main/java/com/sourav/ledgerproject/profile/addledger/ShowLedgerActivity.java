1782f3d6d956f5d5a4f937274b3ae010e6fd6a37
subhajit-ljee <sdey453@gmail.com>
2021-01-26 14:43:47 +0530

    initial commit

--- app/src/main/java/com/sourav/ledgerproject/profile/addledger/ShowLedgerActivity.java
+++ app/src/main/java/com/sourav/ledgerproject/profile/addledger/ShowLedgerActivity.java
@@ -1,11 +1,21 @@
 package com.sourav.ledgerproject.profile.addledger;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 
@@ -18,7 +28,7 @@ import com.sourav.ledgerproject.profile.addledger.view.ShowVoucherLedgerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShowLedgerActivity extends AppCompatActivity {
+public class ShowLedgerActivity extends AppCompatActivity implements ShowVoucherLedgerAdapter.OnItemListener {
 
     private static final String TAG = "ShowLedgerActivity";
 
@@ -35,6 +45,13 @@ public class ShowLedgerActivity extends AppCompatActivity {
     static int closing_bal_cr;
 
     Toolbar voucher_list_toolbar;
+
+    ShowVoucherLedgerAdapter showVoucherLedgerAdapter;
+
+    //List<VoucherLedger> longClickedVoucherLedgerList;
+
+    String opening_amt_print;
+    String closing_amt_print;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +102,7 @@ public class ShowLedgerActivity extends AppCompatActivity {
                         op_bal = snapshot.getString("opening_balance");
                     }
                     opening_balance.setText(op_bal+".00");
+                    this.opening_amt_print = op_bal;
                 });
 
     }
@@ -107,10 +125,11 @@ public class ShowLedgerActivity extends AppCompatActivity {
                         ShowLedgerActivity.voucher_client_list.add(voucherLedger);
                     }
                     Log.d(TAG,"Inside collection: voucher_client_list: "+voucher_client_list);
-                    ShowVoucherLedgerAdapter showVoucherLedgerAdapter = new ShowVoucherLedgerAdapter();
+                    this.showVoucherLedgerAdapter = new ShowVoucherLedgerAdapter(this);
                     showVoucherLedgerAdapter.setList(voucher_client_list);
 
                     recyclerView.setAdapter(showVoucherLedgerAdapter);
+                    Log.d(TAG,"Outside collection: voucher_client_list: "+voucher_client_list);
                 });
 
         Log.d(TAG,"Outside collection: voucher_client_list: "+voucher_client_list);
@@ -228,6 +247,7 @@ public class ShowLedgerActivity extends AppCompatActivity {
                                         String valueofclosing_amount = String.valueOf(closing_bal_cr);
                                         closing_amount.setText(valueofclosing_amount+".00");
 
+                                        this.closing_amt_print = valueofclosing_amount;
                                     });
                         }
                     }
@@ -235,5 +255,51 @@ public class ShowLedgerActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.show_voucher_page_menu,menu);
+        return true;
+    }
 
+    @Override
+    public void onItemClick(int position) {
+        List<VoucherLedger> vouchlist = this.showVoucherLedgerAdapter.getList();
+        Log.d(TAG,"item clicked position: "+vouchlist.get(position));
+        //this.longClickedVoucherLedgerList = vouchlist;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        PrintVoucher printVoucher = new PrintVoucher();
+        switch (item.getItemId()){
+            case R.id.menuPrint:
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setMessage("Sure want to Make PDF")
+                        .setPositiveButton("confirm", (dialog, which) -> {
+
+                            ActivityCompat.requestPermissions(this,new String[]{
+                                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+                            }, PackageManager.PERMISSION_GRANTED);
+
+                            printVoucher.setOpening_balance(opening_amt_print);
+                            printVoucher.setClosing_balance(closing_amt_print);
+                            printVoucher.setVoucher_list(this.showVoucherLedgerAdapter.getList());
+                            printVoucher.setClient_name(getIntent().getStringExtra("client_name"));
+                            printVoucher.makePrintPdf();
+                        })
+                        .setNegativeButton("cancel",null)
+                        .create();
+                alertDialog.show();
+                break;
+            default:
+                Log.d(TAG,"no menu selected");
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
